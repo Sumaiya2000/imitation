eb9f1c5cf42ddec9190c0c0c44f72ded135cdca5
Sumaiya2000 <124509166+Sumaiya2000@users.noreply.github.com>
2023-10-12 22:18:53 +0600

    Update serialize.py

--- src/imitation/rewards/serialize.py
+++ src/imitation/rewards/serialize.py
@@ -6,8 +6,8 @@ import numpy as np
 import torch as th
 from stable_baselines3.common.vec_env import VecEnv
 
-from imitation.rewards import reward_function, reward_nets
-from imitation.util import registry, util
+from imitation.src.imitation.rewards import reward_function, reward_nets
+from imitation.src.imitation.util import registry, util
 
 # TODO(sam): I suspect this whole file can be replaced with th.load calls. Try
 # that refactoring once I have things running.
